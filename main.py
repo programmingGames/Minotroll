61879cc3ecbabcaecf75e76824409ec2491d4ac8
Roberto-04-dev <true4321king@gmail.com>
2020-11-11 19:45:06 -0100

    Post firts painel

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from data.start import Name as nm
-# Teste
-nm.printer()
+import pygame 
+from sys import exit
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+        
+    pygame.display.update()
