dd4345648edf9e674c8b105ce18abeaff8ed3988
Roberto-04-dev <true4321king@gmail.com>
2020-11-19 12:35:20 -0100

    Comentando o modulo data para ser utilizado de guia

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,10 @@
+# data base pre-defenida padrao para armazenar dados ppor enquanto
 class Hysto:
+    # Aqui a iniciação da instancia
     def __inti__(self):
+        # Tens de ir a historia do jogo e copiar paragrafos de texto e colocar em uma posição no vector
         self.text = [[""],
+                        # exemplo ["era uma vez tata tata tata ...."]
                      [""],
                      [""],
                      [""],
@@ -12,7 +16,13 @@ class Hysto:
                      [""],
                      [""]
                      ]
+        # Fazer isso ate a historia terminal e depos contar quantos vetores deu 
+        # e defenir o numero de funções com o conteuda do numero de def de partes
+        # expmplo 5 vetores sera igual a 5 def de partes 
+        # cada def parts returnara o vector contido nessa parte
     def part1(self):
+        # exmplo:
+        # return self.text[1]
         pass
     def part2(self):
         pass
