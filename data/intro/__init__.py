0cc5bf702bff7037baabd02a052dac2a77fb007c
Roberto Carlos <74203966+RobertoCarlosMedina@users.noreply.github.com>
2020-11-23 11:40:06 -0100

    Update __init__.py
    setting intro display

--- data/intro/__init__.py
+++ data/intro/__init__.py
@@ -1,28 +1,34 @@
 import pygame 
 from data import intro
 from data.backgrounds import Backgound as Back
-
+from data import Hysto 
 class Intro(object):
-    def __init__(self, screen, nrImage, menuEsc, nivel):
+    def __init__(self, screen, nrImage):
+        self.background = Back(nrImage)
         self.screen = screen
-        self.time = 0
-        self.background = Back(nrImage, menuEsc, nivel)
+        self.history = Hysto()
         self.painel = pygame.image.load("resources/image/menu/initial_menu/painel.png").convert_alpha()
         self.title = pygame.image.load("resources/image/title/MinoTrolls1.png").convert_alpha()
-        self.font = pygame.font.SysFont(None, 32)
-        self.text1 = "jknfjkwn"
-        # self.parts =
-        
+        self.font = pygame.font.SysFont("Arial", 12,1)
+        self.timeout = 0
+
 
     def introDisplay(self):
-        self.background.settingBackground(self.screen)
-        self.screen.blit(self.painel, (105, 70))
-        self.screen.blit(self.title, (210, 10))
 
-
-        ##  write code 
-        text_surf = self.font.render(self.text1, True, (0, 0, 0))
-        self.screen.blit(text_surf, (200, 200))
-
-        return 4
-        # pygame.time.set_timer(self.NEW_BASIC_SOUL, 3000)
+        tx, ty = 165, 120
+        for paragrafo in self.history.historyData():
+            self.background.settingBackground(self.screen)
+            self.screen.blit(self.painel, (105, 70))
+            self.screen.blit(self.title, (210, 10))
+            if(self.timeout >= 50):
+                    self.screen.fill((0, 0, 0))
+                    self.timeout = 0
+            for line in paragrafo:
+                line = self.font.render(line, True, (0, 0,0))
+                self.screen.blit(line, (tx, ty))
+                ty += 15
+            #parControl += 1
+            ty=165
+        self.timeout += 1
+        print(self.timeout)
+        return 3
